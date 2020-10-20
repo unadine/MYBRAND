2fd0800824887ca762a0f281a57c0b49e5645048
umunadine <umutesinad3@gmail.com>
2020-10-20 14:22:25 +0200

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,4 @@ const openNav=() =>{
 const closeNav=() =>{
   document.getElementById("navigate").style.width = "0"; 
 }
-
-
-
-
-
-
-
-//  const result=() =>{
-//   document.addEventListener("click", () =>{
-//         document.getElementById("navigate").style.width = "100%";
-//         document.getElementById("navigate").style.height = "100%";
-//     }); 
-//   }
-//   document.getElementById("open").innerHTML=result();
-
-
-
-  // var close=() =>{
-  // document.addEventListener("click", () =>{
-  //   document.getElementById("navigate").style.width = "0";
-
-  // console.log("fde");
-  // }); 
-  // }
-  //  document.getElementById("close").innerHTML=close();
-
   
