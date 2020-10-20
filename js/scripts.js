ef4dd27aa58ca4b456ecbfcc3ad0e1cf4c4362ea
umunadine <umutesinad3@gmail.com>
2020-10-20 14:21:01 +0200

    changes

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const openNav=() =>{
+  document.getElementById("navigate").style.width = "100%";
+  document.getElementById("navigate").style.height = "100%";
+}
+const closeNav=() =>{
+  document.getElementById("navigate").style.width = "0"; 
+}
+
+
+
+
+
+
+
+//  const result=() =>{
+//   document.addEventListener("click", () =>{
+//         document.getElementById("navigate").style.width = "100%";
+//         document.getElementById("navigate").style.height = "100%";
+//     }); 
+//   }
+//   document.getElementById("open").innerHTML=result();
+
+
+
+  // var close=() =>{
+  // document.addEventListener("click", () =>{
+  //   document.getElementById("navigate").style.width = "0";
+
+  // console.log("fde");
+  // }); 
+  // }
+  //  document.getElementById("close").innerHTML=close();
+
+  
